25378d777ccbdd58947f4b32d0190b356c7d9c83
M. Kozlik <malkak@skyvar.co.il>
2025-11-24 09:45:24 +0200

    Try  fail zap

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 // אין שימוש ב-helmet או כותרות אבטחה
-app.get('/', (req, res) => {
-  res.send('<h1>Insecure Page</h1>');
-});
+app.get('/xss', (req, res) => {
+    const q = req.query.q;
+    res.send(`<div>${q}</div>`);
+  });
 
 app.listen(8080, '0.0.0.0');
